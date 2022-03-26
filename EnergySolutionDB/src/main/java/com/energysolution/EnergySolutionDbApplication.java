2992bf3c411dbadfd76b1ff88ade2dd68f20cb2d
KimHuiju <gmlwn7245@naver.com>
2022-03-27 01:44:10 +0900

    Create Project

---
+++ EnergySolutionDB/src/main/java/com/energysolution/EnergySolutionDbApplication.java
@@ -0,0 +1,13 @@
+package com.energysolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergySolutionDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergySolutionDbApplication.class, args);
+	}
+
+}
