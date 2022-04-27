9e6b735d26243015dfbeb4228dde9c820b22ce2d
KimHuiju <gmlwn7245@naver.com>
2022-04-27 13:42:28 +0900

    Add RandomPassword Generation Function

--- EnergySolutionDB/src/main/java/com/energysolution/service/UserService.java
+++ EnergySolutionDB/src/main/java/com/energysolution/service/UserService.java
@@ -3,6 +3,9 @@ package com.energysolution.service;
 import java.util.HashMap;
 import java.util.List;
 
+import java.security.SecureRandom; 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -57,17 +60,41 @@ public class UserService implements UserServiceInterface {
 		
 		UserDTO userDTO = userMapper.FindUserPW(findUserPWMap);
 		
-
-
 		//임시 비밀번호 발급
-		String temp = "tempPW002";
+		String tempPW = getRandomPassword(10);
 		HashMap<String, String> updateMap = new HashMap<String, String>();
 		updateMap.put("UserId", userDTO.getUserId());
-		updateMap.put("newPW", temp);
+		updateMap.put("newPW", tempPW);
 		String res = updateUser(updateMap);
 		
 		return res;
 	}
+	
+	//임시 비밀번호 발급
+	public String getRandomPassword(int size) {
+		char[] charSet = new char[] { 
+				'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+				'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+				'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+				'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 
+				'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+				'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+				'y', 'z', '!', '@', '#', '$', '%', '^', '&' }; 
+		StringBuffer sb = new StringBuffer(); 
+		SecureRandom sr = new SecureRandom(); 
+		sr.setSeed(new Date().getTime()); 
+		int idx = 0; 
+		int len = charSet.length;
+		for (int i=0; i<size; i++) {
+			// idx = (int) (len * Math.random()); 
+			idx = sr.nextInt(len); 
+			// 강력한 난수를 발생시키기 위해 SecureRandom을 사용 
+			sb.append(charSet[idx]); 
+			} 
+		return sb.toString(); 
+		
+	}
+
 	
 	
 	//로그인 하기
